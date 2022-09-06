2cb231afe45448e39321ba8935b907f15313d808
Nuria García <nuriag023@thecorner.cat>
2022-09-06 19:07:37 +0100

    Agregar archivos de proyecto.

---
+++ Datos/Class1.cs
@@ -0,0 +1,129 @@
+﻿using Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+
+    public class Class1
+    {
+
+        private static NorthWindTuneadoDbContext _dbContext = null;
+
+        //constructor
+        public Class1()
+            {
+            if (_dbContext== null)
+            {
+                _dbContext = new NorthWindTuneadoDbContext();
+            }
+             
+    }
+
+        //metodo
+        public IList<Categoria> DevuelveCategorias()
+        {
+            IList<Categoria> categorias = null;
+            categorias = _dbContext.Categoria.ToList();
+
+            return categorias;
+        }
+
+        public bool GuardarCambios(Categoria categoria)
+        {
+            bool ok = false;
+            int respuesta = 0;
+
+            try
+            {
+                _dbContext.Categoria.Add(categoria);
+                respuesta = _dbContext.SaveChanges();
+
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+
+        }
+
+
+
+
+        public bool GuardarRegistro(int id, string texto)
+        {
+            bool ok = false;
+            Categoria categoria = null;
+            //Categoria categoria = _dbContext.Categoria.Find(id);
+            categoria = _dbContext.Categoria.Where(x=>x.CategoryID== id).FirstOrDefault();
+            categoria.CategoryName = texto;
+
+            try
+            {
+                int respuesta = 0;
+                respuesta = _dbContext.SaveChanges();
+
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+
+        }
+
+
+
+        public bool EliminarRegistro(int id)
+        {
+            bool ok = false;
+
+
+            Categoria categoria = null;
+
+            categoria = _dbContext.Categoria.Where(x => x.CategoryID == id).FirstOrDefault();
+
+            _dbContext.Categoria.Remove(categoria);
+
+            try
+            {
+                int respuesta = 0;
+                respuesta = _dbContext.SaveChanges();
+
+                if (respuesta > 0)
+                {
+                    ok = true;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return ok;
+
+        }
+
+
+    }
+}
---
+++ Principal/Program.cs
@@ -0,0 +1,149 @@
+﻿using Datos;
+using Datos.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.AnyadeCategoria();
+            program.Comprobar();
+            program.Modificar();
+            program.Comprobar();
+            program.Eliminar();
+            program.Comprobar();
+
+        }
+
+        //metodo
+        //INSERT CREATE
+        private void AnyadeCategoria()
+        {
+            Class1 class1 = null;
+            class1 = new Class1();
+
+            IList<Categoria> categorias = null;
+            categorias = class1.DevuelveCategorias();
+
+            Categoria categoria = null;
+            categoria = new Categoria();
+
+            categoria.CategoryName = "Zapatos";
+            categoria.Description = "Accesorio ropa";
+
+            categorias.Add(categoria);
+
+            bool ok = false;
+
+            ok = class1.GuardarCambios(categoria);
+
+            Console.WriteLine("El registro ha sido guardado " + ok);
+
+            Console.ReadLine();
+
+
+
+
+        }
+
+        //SELECT O READ
+        private void Comprobar()
+        {
+        Class1 class1 = new Class1();
+        IList<Categoria> categorias = class1.DevuelveCategorias();
+
+        foreach (Categoria categoria in categorias)
+            {
+            Console.WriteLine(categoria.CategoryName);
+            
+            }
+    Console.ReadLine();
+        }
+
+
+        //UPDATE
+        private void Modificar()
+        {
+            Class1 class1 = new Class1();
+            IList<Categoria> categorias = class1.DevuelveCategorias();
+
+
+
+            foreach (Categoria categoria in categorias)
+            {
+                Console.WriteLine(categoria.CategoryID + " " + categoria.CategoryName);
+
+            }
+            Console.WriteLine("Dime el id del regitro a modificar");
+            string texto = Console.ReadLine();
+            int id = 0;
+            if (int.TryParse(texto, out id) ==true)
+            {
+                id = int.Parse(texto);
+            }
+
+            Console.WriteLine("Ahora dime el nuevo nombre");
+            
+            texto = Console.ReadLine();
+
+            bool ok = false;
+
+            ok = class1.GuardarRegistro(id, texto);
+
+            Console.WriteLine("El resultado de la modificacion es " + ok);
+
+            Console.ReadLine();
+             
+        }
+
+        //DELETE
+
+        private void Eliminar()
+        {
+            Class1 class1 = new Class1();
+            IList<Categoria> categorias = class1.DevuelveCategorias();
+
+
+
+            foreach (Categoria categoria in categorias)
+            {
+                Console.WriteLine(categoria.CategoryID + " " + categoria.CategoryName);
+
+            }
+            Console.WriteLine("Dime el id del registro a eliminar");
+            string texto = Console.ReadLine();
+            int id = 0;
+            if (int.TryParse(texto, out id) == true)
+            {
+                id = int.Parse(texto);
+            }
+
+  
+
+            bool ok = false;
+
+            ok = class1.EliminarRegistro(id);
+
+            Console.WriteLine("El resultado de la eliminación es " + ok);
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
